30f8abb3ff265c633a600fc99aab974a981244ea
amin <shafiee.mohamin@gmail.com>
2023-08-06 23:14:05 -0400

    building contract completed.

--- backend/main.py
+++ backend/main.py
@@ -2,13 +2,16 @@ from flask import Flask
 from flask import request
 from flask import jsonify
 from typing import Any
+from flask_cors import CORS
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 
 DEFAULT_DATA_KEYS = 'contract'
 
-def checkDataFromat(content: Any):
+def checkDataFromat(content: Any) -> str:
     if type(content) is not dict:
         raise Exception("Content needs to be dictionary")
 
@@ -18,17 +21,32 @@ def checkDataFromat(content: Any):
         raise Exception(f"{DEFAULT_DATA_KEYS} needs to be present in the data.")
     
     contract = content.get(DEFAULT_DATA_KEYS)
+    return contract
 
-    print(contract)
+def createContractFormat(contractStr: str) -> str:
+    librsFilePath = os.path.join(os.path.dirname(__file__), 'TokenBuild', 'src', 'lib.rs')
+    print(librsFilePath)
+    with open(librsFilePath, 'w') as file:
+        file.write(contractStr)
 
+    return librsFilePath
+
+def compileContract():
+    srcPath = os.path.join(os.path.dirname(__file__), 'TokenBuild')
+    
+    os.system(f'cd {srcPath} && cargo partisia-contract build --release')
 
 @app.route('/build', methods=['POST'])
 def buildContract():
     content = request.get_json()
     try:
-        checkDataFromat(dict(content))
+        contract = checkDataFromat(dict(content))
+        _ = createContractFormat(contract)
+        compileContract()
     except Exception as ex:
         return jsonify({'message': ex.args[1]}, 400)
+    
+    return jsonify({'message': 'ok'}, 200)
 
 
 
